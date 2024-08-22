940d384bb4e271ed9ff7a358ee66d653a42fa168
Nikhil Deshmane <iamdeshmanenl@gmail.com>
2024-08-22 22:59:21 +0530

    Update AssignmentCountdown.dart

--- AssignmentCountdown.dart
+++ AssignmentCountdown.dart
@@ -2,23 +2,10 @@ import"dart:core";
 void main(){
   int noDays=7;
   while(noDays>=0){
-    if(noDays==7){
-      print("$noDays days Remaining");
-    }else if(noDays==6){
-      print("$noDays days Remaining");
-    }else if(noDays==5){
-      print("$noDays days Remaining");
-    }else if(noDays==4){
-      print("$noDays days Remaining");
-    }else if(noDays==3){
-      print("$noDays days Remaining");
-    }else if(noDays==2){
-      print("$noDays days Remaining");
-    }else{
-      print("$noDays days Ryemaining");
-    }
+    if(noDays==0){
+      print("0 days Remaining");
+    }else {
+      print("$noDays days Remaning");
     --noDays;
-    
-    
   }
 }
